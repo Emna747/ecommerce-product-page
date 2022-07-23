10d7d39c053d70962074186fbe31100f81de2afb
Emna <84625792+Emna747@users.noreply.github.com>
2022-07-23 18:42:18 +0300

    JS initial commit

---
+++ products.js
@@ -0,0 +1,272 @@
+var productItem = [
+  {
+    productName: "Wooden Dinosaurs Set",
+    price: "36.99",
+  },
+  {
+    productName: "Wooden Kitchen Set",
+    price: "42.89",
+  },
+  {
+    productName: "Colorful Loops",
+    price: "19.89",
+  },
+  {
+    productName: "Wooden Alphabet Snake",
+    price: "22.99",
+  },
+  {
+    productName: "Stuffed Lion Toy",
+    price: "7.99",
+  },
+  {
+    productName: "Pearls Rainbow Toy",
+    price: "39.89",
+  },
+  {
+    productName: "Baby Wool Toy",
+    price: "9.89",
+  },
+  {
+    productName: "Wooden Leafs Set ",
+    price: "27.99",
+  },
+  {
+    productName: "Wood Animals Puzzles",
+    price: "17.99",
+  },
+  {
+    productName: "Baby Wooden Set",
+    price: "30.99",
+  },
+  {
+    productName: "Whale Puzzle Set",
+    price: "16.89",
+  },
+  {
+    productName: "Alphabet Wooden Set",
+    price: "22.89",
+  },
+  {
+    productName: "Storage Boxes ",
+    price: "19.99",
+  },
+  {
+    productName: "Zoo Animals Set",
+    price: "22.89",
+  },
+  {
+    productName: "Dinosaur Rocking Chair ",
+    price: "74.89",
+  },
+  {
+    productName: "Red Dress",
+    price: "50.99",
+  },
+  {
+    productName: "Colors Pattern Dress",
+    price: "64.99",
+  },
+  {
+    productName: "Boy Blue Set",
+    price: "87.99",
+  },
+  {
+    productName: "Patterned T-Shirt Flannel",
+    price: "24.55",
+  },
+  {
+    productName: "Paw Patrol Set",
+    price: "55.99",
+  },
+  {
+    productName: "Pinky New Born Set",
+    price: "13.99",
+  },
+  {
+    productName: "White Cotton Dress",
+    price: "29.99",
+  },
+  {
+    productName: "Dark blue Flannel",
+    price: "32.99",
+  },
+  {
+    productName: "Aqua Girl Set",
+    price: "56.99",
+  },
+  {
+    productName: "Pink Skirt ",
+    price: "18.99",
+  },
+  {
+    productName: "Green Dinosaur Set",
+    price: "60.99",
+  },
+  {
+    productName: "Beige Rain Coat",
+    price: "74.99",
+  },
+  {
+    productName: "Baby Bath Cap",
+    price: "20.99",
+  },
+  {
+    productName: "Cotton T-Shirt",
+    price: "16.99",
+  },
+  {
+    productName: "Disney Princess Dress",
+    price: "37.99",
+  },
+  {
+    productName: "White Cotton Flannel",
+    price: "18.99",
+  },
+  {
+    productName: "Boy Lego Set",
+    price: "45.99",
+  },
+  {
+    productName: "Cotton T-Shirt",
+    price: "19.99",
+  },
+  {
+    productName: "Polka Dot Bbay Shower Cap",
+    price: "22.99",
+  },
+  {
+    productName: "Pink Dress",
+    price: "55.99",
+  },
+  {
+    productName: "Cotton Flower Set",
+    price: "30.99",
+  },
+  {
+    productName: "Blue T-Shirt",
+    price: "18.99",
+  },
+  {
+    productName: "Green Girl Dress",
+    price: "56.99",
+  },
+  {
+    productName: "Grey Cotton Flannel",
+    price: "25.99",
+  },
+  {
+    productName: "Baby and Toddler Crib",
+    price: "300",
+  },
+  {
+    productName: "Baby Wood Crib ",
+    price: "289",
+  },
+  {
+    productName: "Closet Items",
+    price: "450",
+  },
+  {
+    productName: "White Wooden wardrobe",
+    price: "189",
+  },
+  {
+    productName: "Sofa Chair",
+    price: "115",
+  },
+  {
+    productName: "Long Sofa Chair",
+    price: "220",
+  },
+  {
+    productName: "Rectangular Low Wardrobe",
+    price: "190",
+  },
+  {
+    productName: "Circular Wooden Crib",
+    price: "490",
+  },
+  {
+    productName: "Book Holder Set",
+    price: "55",
+  },
+  {
+    productName: "Binky Jar",
+    price: "25",
+  },
+  {
+    productName: "Indoor Small Tipi",
+    price: "110",
+  },
+  {
+    productName: "Rectangular Grey Wardrobe",
+    price: "189",
+  },
+  {
+    productName: "Study desk",
+    price: "299",
+  },
+  {
+    productName: "Blue Whale Wardrobe",
+    price: "210",
+  },
+  {
+    productName: "Decorative Lamps",
+    price: "32",
+  },
+  {
+    productName: "Fruits and Vegetables Book",
+    price: "6.99",
+  },
+  {
+    productName: "Fun with Numbers and Shapes",
+    price: "9.99",
+  },
+  {
+    productName: "Coloring Book ",
+    price: "7.89",
+  },
+  {
+    productName: "Toddler Coloring Book",
+    price: "7.89",
+  },
+  {
+    productName: "Easy Coloring Book",
+    price: "7.89",
+  },
+  { productName: "Rescue at Lake Wild Book", price: "9.99" },
+  {
+    productName: "Charlotte's Web",
+    price: "10.99",
+  },
+  {
+    productName: "My Quiet Imagination",
+    price: "8.99",
+  },
+  {
+    productName: "The Voyage to Magical North",
+    price: "13.99",
+  },
+  {
+    productName: "Peter Pan",
+    price: "10.99",
+  },
+  { productName: "Skycircus", price: "5.89" },
+  {
+    productName: "Susie Won't Back Down",
+    price: "16.99",
+  },
+  {
+    productName: "Red Riding Hood",
+    price: "9.99",
+  },
+  {
+    productName: "Alphabet Book",
+    price: "13.89",
+  },
+  {
+    productName: "Having A Friend",
+    price: "8.99",
+  },
+];
